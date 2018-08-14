0ecb23c60e64f13be7b0b544841fc4680e8f0aa1
Rafael Noriega <rafaelnoriega@Rafaels-MacBook-Pro.local>
2018-08-13 21:54:28 -0700

    Updates

---
+++ Archive/app.js
@@ -0,0 +1,33 @@
+
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+app.use("/assets", express.static("assets"));
+
+var movie = "";
+
+app.get("/", function(req, res){
+	res.render("search");
+});
+
+app.get("/results", function(req,res){
+		//console.log(req.query.search);
+		var search = req.query.search;
+		var url =  "http://www.omdbapi.com/?apikey=thewdb&s=" + search;
+		request( url ,function(error, result, body){
+		if(!error && result.statusCode == 200){
+			var data = JSON.parse(body);
+			//console.log(data);
+			res.render("results",{data:data});
+		}
+		else{
+			console.log("ERROR");
+		}
+	});
+});
+	
+
+app.listen(3000, function(){
+	console.log("server running");
+});
